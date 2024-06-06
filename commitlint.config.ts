c0ae3e6fde7816db6b17dd332c0bde2e755dd3ed
柳问星 <lwx_redstone@163.com>
2024-06-06 20:40:54 +0800

    build(root): change commit lint config

---
+++ commitlint.config.ts
@@ -0,0 +1,19 @@
+import { RuleConfigSeverity, type UserConfig } from "@commitlint/types";
+import scopes from "@commitlint/config-pnpm-scopes";
+
+const Configuration: UserConfig = {
+  extends: [
+    "@commitlint/config-conventional",
+    "@commitlint/config-pnpm-scopes",
+  ],
+  rules: {
+    "scope-empty": [RuleConfigSeverity.Error, "never"],
+    // @ts-ignore
+    "scope-enum": async (ctx) => {
+      const scopeEnum = await scopes.rules["scope-enum"](ctx);
+      return [scopeEnum[0], scopeEnum[1], ["root", ...scopeEnum[2]]];
+    },
+  },
+};
+
+export default Configuration;
